c8a6ea51c28f0cb86071d6a77f7e4013a3df95e7
Daniel Kennedy <fade.rocks@gmail.com>
2021-03-22 22:22:18 +0000

    css and making it nice

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ number, cat, joke }) => {
     return (
-        <div style={{display: "flex", justifyContent: "center", margin: "5rem"}}>
+        <div style={{display: "flex", border: "5px solid #E6E6FA", padding: "2rem", justifyContent: "center", margin: "5rem auto"}}>
             <img src={`http://pngimg.com/uploads/chuck_norris/small/chuck_norris_PNG${number}.png`} width="auto" height="200px" alt="Chuck"/>
             <div>
                 <h3>{cat}</h3>
