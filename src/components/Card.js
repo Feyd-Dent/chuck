6ebfbcdc3f361a2745763dfb747db59f5c07de17
Daniel Kennedy <fade.rocks@gmail.com>
2021-03-28 15:58:39 +0100

    css and responsiveness tweaks

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,16 @@
 import React from "react";
+import './Card.css';
 
 const Card = ({ number, cat, joke }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        border: "5px solid #E6E6FA",
-        padding: "2rem",
-        justifyContent: "center",
-        margin: "5rem auto",
-      }}
-    >
+    <div className="card">
       <img
         src={`http://pngimg.com/uploads/chuck_norris/small/chuck_norris_PNG${number}.png`}
-        width="auto"
-        height="200px"
         alt="Chuck"
       />
       <div>
         <h3>{cat}</h3>
-        <p style={{ padding: "2rem", width: "55ch" }}>{joke}</p>
+        <p style={{ padding: "2rem", maxWidth: "55ch" }}>{joke}</p>
       </div>
     </div>
   );
